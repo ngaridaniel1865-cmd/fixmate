e326bf129b92d6632ce2913c9eb43ef3a437fb52
ngaridaniel1865-cmd <ngaridaniel1865@gmail.com>
2026-09-19 10:35:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -58,7 +58,6 @@ document.getElementById("signInBtn").addEventListener("click", async () => {
 
   message.textContent = "You are now signed in.";
 });
-showView("home");
 const services = [
   {name:"Plumbing", icon:"🚰", desc:"Leaks, taps, toilets, pipes and water issues."},
   {name:"Electrical", icon:"🔌", desc:"Sockets, lights, wiring and electrical faults."},
